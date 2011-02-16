916d31c62b66c0b3d15037d12dab9643419f028e
Ben Konrath <ben@bagu.org>
2011-02-16 11:09:24 +0100

    Disable generateAsync feature of gwt-maven.
    The generated interface does not correctly match the methods in LibGlomService
    and the generated singleton Util inner-class doesn't respect the servlet
    mappings.
    
    * pom.xml: Turn off generateAsync feature.
    * src/main/java/org/glom/web/client/LibGlomServiceAsync.java: Re-add file
    with singleton Util inner-class.

---
+++ src/main/java/org/glom/web/client/LibGlomServiceAsync.java
@@ -0,0 +1,33 @@
+package org.glom.web.client;
+
+import java.util.ArrayList;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
+public interface LibGlomServiceAsync {
+
+	void getGlomDocument(AsyncCallback<GlomDocument> callback);
+
+	void getLayoutListHeaders(String table, AsyncCallback<String[]> callback);
+
+	void getTableData(int start, int length, String table, AsyncCallback<ArrayList<String[]>> callback);
+
+	/**
+	 * Utility class to get the RPC Async interface from client-side code
+	 */
+	public static final class Util {
+		private static LibGlomServiceAsync instance;
+
+		public static final LibGlomServiceAsync getInstance() {
+			if (instance == null) {
+				instance = (LibGlomServiceAsync) GWT.create(LibGlomService.class);
+			}
+			return instance;
+		}
+
+		private Util() {
+			// Utility class should not be instanciated
+		}
+	}
+}
