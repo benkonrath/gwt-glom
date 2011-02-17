45d653840ace831b1eb3a311ab3d02e30fa4b36f
Ben Konrath <ben@bagu.org>
2011-02-17 21:02:37 +0100

    Move LayoutListDataProvider class into LayoutList.java.
    * src/main/java/org/glom/web/client/LayoutList.java:

--- src/main/java/org/glom/web/client/LayoutList.java
+++ src/main/java/org/glom/web/client/LayoutList.java
@@ -19,11 +19,40 @@
 
 package org.glom.web.client;
 
+import java.util.ArrayList;
+
 import com.google.gwt.user.cellview.client.CellTable;
 import com.google.gwt.user.cellview.client.SimplePager;
 import com.google.gwt.user.cellview.client.TextColumn;
+import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.view.client.AsyncDataProvider;
+import com.google.gwt.view.client.HasData;
+import com.google.gwt.view.client.Range;
+
+class LayoutListDataProvider extends AsyncDataProvider<String[]> {
+	@Override
+	protected void onRangeChanged(HasData<String[]> display) {
+		final Range range = display.getVisibleRange();
+
+		final int start = range.getStart();
+		final int length = range.getLength();
+
+		AsyncCallback<ArrayList<String[]>> callback = new AsyncCallback<ArrayList<String[]>>() {
+			public void onFailure(Throwable caught) {
+				// FIXME: need to deal with failure
+				System.out.println("AsyncCallback Failed: LibGlomService.getTableData()");
+			}
+
+			public void onSuccess(ArrayList<String[]> result) {
+				updateRowData(start, result);
+			}
+		};
+
+		OnlineGlomServiceAsync.Util.getInstance().getTableData(start, length, OnlineGlom.getCurrentTableName(), callback);
+	}
+}
 
 public class LayoutList extends Composite {
 
