ccf9b1f4f85ffa1322c5785f8e28b04f39723150
Ben Konrath <ben@bagu.org>
2011-09-23 12:42:50 +0200

    Remove annotations that turn off code formatting in DataItem.
    * src/main/java/org/glom/web/shared/DataItem.java:

--- src/main/java/org/glom/web/shared/DataItem.java
+++ src/main/java/org/glom/web/shared/DataItem.java
@@ -32,14 +32,35 @@ public class DataItem implements Serializable {
 	public DataItem() {
 	}
 
-	// @formatter:off
-	public String getText() { return text;}
-	public void setText(String text) { this.text = text; }
-	public boolean getBoolean() { return bool; }
-	public void setBoolean(boolean bool) { this.bool = bool; }
-	public String getFGColour() { return fgColour; }
-	public void setFGColour(String fgColour) { this.fgColour = fgColour; }
-	public String getBGColour() { return bgColour; }
-	public void setBGColour(String bgColour) { this.bgColour = bgColour; }
-	// @formatter:on
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public boolean getBoolean() {
+		return bool;
+	}
+
+	public void setBoolean(boolean bool) {
+		this.bool = bool;
+	}
+
+	public String getFGColour() {
+		return fgColour;
+	}
+
+	public void setFGColour(String fgColour) {
+		this.fgColour = fgColour;
+	}
+
+	public String getBGColour() {
+		return bgColour;
+	}
+
+	public void setBGColour(String bgColour) {
+		this.bgColour = bgColour;
+	}
 }
