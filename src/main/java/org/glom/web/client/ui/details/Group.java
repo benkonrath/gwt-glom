benkonrath/gwt-glom

commit b1753fd27bd2c4ea189c4c353e0ece92dcc66c2c

Don't explicitly set the height of Portals.
See comments 6 - 10 of this bug for details:

https://bugzilla.gnome.org/show_bug.cgi?id=662930#c6

* src/main/java/org/glom/web/client/ui/details/Portal.java:

src/main/java/org/glom/web/client/ui/details/Portal.java
@@ -57,16 +57,10 @@ public class Portal extends Composite {
 		FlowPanel mainPanel = new FlowPanel();
 		mainPanel.setStyleName("subgroup");
 
-		if (setTitle == true) {
-			mainPanel.setHeight("15.8em"); // the .8 is to match the details-cell bottom margin
-
-			// using the same style as the (sub)group-title and group-contents elements
-			Label title = new Label(layoutItem.getTitle());
-			title.setStyleName(groupTitleOrSubgroupTitle ? "group-title" : "subgroup-title");
-			mainPanel.add(title);
-		} else {
-			mainPanel.setHeight("14.8em"); // the .8 is to match the details-cell bottom margin
-		}
+		// using the same style as the (sub)group-title and group-contents elements
+		Label title = new Label(layoutItem.getTitle());
+		title.setStyleName(groupTitleOrSubgroupTitle ? "group-title" : "subgroup-title");
+		mainPanel.add(title);
 
 		contents.setStyleName("group-contents");
 

commit ebe0c8c3e7b96c622af8ad03a49ca43203951224

Don't add group titles for Portals in Notebooks.
This reverts the previous patch and fixes a bug I introduced with
commit b1753fd27bd2c4ea189c4c353e0ece92dcc66c2c .

* src/main/java/org/glom/web/client/ui/details/Group.java:
* src/main/java/org/glom/web/client/ui/details/Portal.java:

src/main/java/org/glom/web/client/ui/details/Group.java
@@ -126,7 +126,7 @@ public class Group extends Composite {
 			if (layoutItem instanceof LayoutItemPortal) {
 
 				// create a Portal
-				Portal portal = new Portal((LayoutItemPortal) layoutItem);
+				Portal portal = new Portal((LayoutItemPortal) layoutItem, addGroupTitle);
 				portals.add(portal);
 				return portal;
 

src/main/java/org/glom/web/client/ui/details/Portal.java
@@ -45,16 +45,20 @@ public class Portal extends Composite {
 	 * 
 	 * @param layoutItem
 	 *            The DTO that holds the Portal layout information
+	 * @param setTitle
+	 *            true if the title should be set, false otherwise
 	 */
-	public Portal(LayoutItemPortal layoutItemPortal) {
+	public Portal(LayoutItemPortal layoutItemPortal, boolean setTitle) {
 		layoutItem = layoutItemPortal;
 
 		FlowPanel mainPanel = new FlowPanel();
 		mainPanel.setStyleName("subgroup");
 
-		Label title = new Label(layoutItem.getTitle());
-		title.setStyleName("subgroup-title");
-		mainPanel.add(title);
+		if (setTitle) {
+			Label title = new Label(layoutItem.getTitle());
+			title.setStyleName("subgroup-title");
+			mainPanel.add(title);
+		}
 
 		contents.setStyleName("group-contents");
 