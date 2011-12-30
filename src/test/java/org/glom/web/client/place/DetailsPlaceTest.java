5325753d8ca0fc2993245fc0eae6d0603e76375e
Murray Cumming <murrayc@murrayc.com>
2011-12-30 23:34:26 +0100

    whitespace cleanup

--- src/test/java/org/glom/web/client/place/DetailsPlaceTest.java
+++ src/test/java/org/glom/web/client/place/DetailsPlaceTest.java
@@ -4,62 +4,64 @@ import junit.framework.TestCase;
 import junit.framework.Assert;
 
 public class DetailsPlaceTest extends TestCase {
- 
+
 	public DetailsPlaceTest() {
 	}
- 
-	public void testGetPlaceNoParameters()  {
+
+	public void testGetPlaceNoParameters() {
 		checkTokenWithoutParameters("");
 		checkTokenWithoutParameters("something");
 		checkTokenWithoutParameters("list:a=1");
 		checkTokenWithoutParameters("value1=123");
 	}
 
-	public void testGetPlaceParameters()  {
+	public void testGetPlaceParameters() {
 		// Create a DetailsPlace, testing getPlace():
-                final String documentId = "somedocument";
+		final String documentId = "somedocument";
 		final String tableName = "sometable";
 		final String primaryKeyValue = "123";
-		DetailsPlace place = getDetailsPlaceFromToken("document=" + documentId + "&table=" + tableName + "&value=" + primaryKeyValue);
+		DetailsPlace place = getDetailsPlaceFromToken("document=" + documentId + "&table=" + tableName + "&value="
+				+ primaryKeyValue);
 		checkParameters(place, documentId, tableName, primaryKeyValue);
 
 		// Recreate it, testing getToken(),
 		// checking that the same parameters are read back:
-                DetailsPlace.Tokenizer tokenizer = new DetailsPlace.Tokenizer();
+		DetailsPlace.Tokenizer tokenizer = new DetailsPlace.Tokenizer();
 		final String token = tokenizer.getToken(place);
-                place = getDetailsPlaceFromToken(token);
+		place = getDetailsPlaceFromToken(token);
 		checkParameters(place, documentId, tableName, primaryKeyValue);
 	}
 
-	private void checkParameters(DetailsPlace place, final String documentID, final String tableName, final String primaryKeyValue) {
-		Assert.assertTrue( place != null );
+	private void checkParameters(DetailsPlace place, final String documentID, final String tableName,
+			final String primaryKeyValue) {
+		Assert.assertTrue(place != null);
 
-		Assert.assertEquals(documentID, place.getDocumentID() );
-		Assert.assertEquals(tableName, place.getTableName() );
-		Assert.assertEquals(primaryKeyValue, place.getPrimaryKeyValue().getUnknown() );
+		Assert.assertEquals(documentID, place.getDocumentID());
+		Assert.assertEquals(tableName, place.getTableName());
+		Assert.assertEquals(primaryKeyValue, place.getPrimaryKeyValue().getUnknown());
 	}
 
-        private DetailsPlace getDetailsPlaceFromToken(final String token) {
+	private DetailsPlace getDetailsPlaceFromToken(final String token) {
 		DetailsPlace.Tokenizer tokenizer = new DetailsPlace.Tokenizer();
 		DetailsPlace place = tokenizer.getPlace(token);
-		Assert.assertTrue( place != null );
+		Assert.assertTrue(place != null);
 		return place;
 	}
 
 	private void checkTokenWithoutParameters(final String token) {
 		DetailsPlace place = getDetailsPlaceFromToken(token);
 
-		Assert.assertTrue( place.getDocumentID() != null );
-		Assert.assertTrue( place.getDocumentID().isEmpty() );
+		Assert.assertTrue(place.getDocumentID() != null);
+		Assert.assertTrue(place.getDocumentID().isEmpty());
 
-		Assert.assertTrue( place.getTableName() != null );
-		Assert.assertTrue( place.getTableName().isEmpty() );
+		Assert.assertTrue(place.getTableName() != null);
+		Assert.assertTrue(place.getTableName().isEmpty());
 
-		Assert.assertTrue( place.getPrimaryKeyValue() != null );
-		Assert.assertTrue( place.getPrimaryKeyValue().isEmpty() );
-		Assert.assertEquals(null, place.getPrimaryKeyValue().getUnknown() );
-		Assert.assertEquals(0.0, place.getPrimaryKeyValue().getNumber() );
-		Assert.assertEquals(null, place.getPrimaryKeyValue().getText() );
+		Assert.assertTrue(place.getPrimaryKeyValue() != null);
+		Assert.assertTrue(place.getPrimaryKeyValue().isEmpty());
+		Assert.assertEquals(null, place.getPrimaryKeyValue().getUnknown());
+		Assert.assertEquals(0.0, place.getPrimaryKeyValue().getNumber());
+		Assert.assertEquals(null, place.getPrimaryKeyValue().getText());
 	}
 
 }
