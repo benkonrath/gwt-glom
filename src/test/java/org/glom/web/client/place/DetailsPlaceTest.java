bfc4bf86f427ce784d78995c21f9ddcf6b238315
Murray Cumming <murrayc@murrayc.com>
2011-12-30 23:27:09 +0100

    DetailsPlace test: Also test getToken() and recreation via getPlace().
    * src/test/java/org/glom/web/client/place/DetailsPlaceTest.java:
    testGetPlaceParameters(): Get the tokens from the DetailsPlace and
    recreate it, testing the recreated DetailsPlace for the same parameter
    values.

--- src/test/java/org/glom/web/client/place/DetailsPlaceTest.java
+++ src/test/java/org/glom/web/client/place/DetailsPlaceTest.java
@@ -16,11 +16,27 @@ public class DetailsPlaceTest extends TestCase {
 	}
 
 	public void testGetPlaceParameters()  {
-		DetailsPlace place = getDetailsPlaceFromToken("document=somedocument&table=sometable&value=123");
+		// Create a DetailsPlace, testing getPlace():
+                final String documentId = "somedocument";
+		final String tableName = "sometable";
+		final String primaryKeyValue = "123";
+		DetailsPlace place = getDetailsPlaceFromToken("document=" + documentId + "&table=" + tableName + "&value=" + primaryKeyValue);
+		checkParameters(place, documentId, tableName, primaryKeyValue);
 
-		Assert.assertEquals("somedocument", place.getDocumentID() );
-		Assert.assertEquals("sometable", place.getTableName() );
-		Assert.assertEquals("123", place.getPrimaryKeyValue().getUnknown() );
+		// Recreate it, testing getToken(),
+		// checking that the same parameters are read back:
+                DetailsPlace.Tokenizer tokenizer = new DetailsPlace.Tokenizer();
+		final String token = tokenizer.getToken(place);
+                place = getDetailsPlaceFromToken(token);
+		checkParameters(place, documentId, tableName, primaryKeyValue);
+	}
+
+	private void checkParameters(DetailsPlace place, final String documentID, final String tableName, final String primaryKeyValue) {
+		Assert.assertTrue( place != null );
+
+		Assert.assertEquals(documentID, place.getDocumentID() );
+		Assert.assertEquals(tableName, place.getTableName() );
+		Assert.assertEquals(primaryKeyValue, place.getPrimaryKeyValue().getUnknown() );
 	}
 
         private DetailsPlace getDetailsPlaceFromToken(final String token) {
