40bad9724b89fb01defaecb3149251a0026c17ea
Murray Cumming <murrayc@murrayc.com>
2012-01-01 01:58:26 +0100

    Tests: Added the beginnings of a test using gwt-test-utils.
    * pom.xml: Add dependencies on gwt-test-utils and easymock.
    * src/test/resources/META-INF/gwt-test-utils.properties: Add this file
    which tells gwt-test-utils what class will be tested.
    * src/test/java/org/glom/web/client/GwtTestOnlineGlom.java:
    Add a simple (but empty) test case. One class, used by the OnlineGlom
    class, is mocked so that it can be created. However, I am not sure
    why only this class needs to be mocked.
    
    Note that mockito seems more popular, and clearer, than easymock,
    but I have not got that working yet. It might be a matter of the
    mockito version.

---
+++ src/test/java/org/glom/web/client/GwtTestOnlineGlom.java
@@ -0,0 +1,63 @@
+package org.glom.web.client;
+
+import com.google.gwt.event.dom.client.KeyCodes;
+import com.google.gwt.user.client.Event;
+
+import com.octo.gwt.test.Mock;
+import com.octo.gwt.test.GwtTestWithEasyMock;
+import com.octo.gwt.test.utils.events.Browser;
+import com.octo.gwt.test.utils.events.EventBuilder;
+
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.Assert;
+
+import org.easymock.EasyMock;
+
+public class GwtTestOnlineGlom extends GwtTestWithEasyMock {
+
+	/** Tell gwt-test-utils to use this mock when trying to create this class via GWT.create() in OnlineGlom.
+         * TODO: Why does only this class need to be mocked?
+         */
+	@Mock
+	private org.glom.web.client.mvp.AppPlaceHistoryMapper mockAppPlaceHistoryMapper;
+
+	private OnlineGlom app;
+
+
+	@Test
+	public void testSomething() {
+		assertTrue(true);
+		/* TODO: For instance:
+		// Arrange
+ 		Browser.fillText(app.nameField, "123");
+
+		// Act
+		Browser.click(app.sendButton);
+
+		// Assert
+		assertFalse(app.dialogBox.isShowing());
+		assertEquals("Please enter at least four characters",
+			app.errorLabel.getText());
+		*/
+	}
+
+	@Override
+	public String getModuleName() {
+		return "org.glom.web.OnlineGlom";
+	}
+
+	@Before
+	public void beforeOnlineGlom() {
+		app = new OnlineGlom();
+		app.onModuleLoad();
+
+		/* TODO: For instance,
+ 		// Some pre-assertions
+		assertFalse(app.dialogBox.isShowing());
+ 		assertEquals("", app.errorLabel.getText());
+		*/
+	}
+
+}
