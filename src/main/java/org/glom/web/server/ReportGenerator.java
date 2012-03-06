c7bcb6ebe0746f4cc4c8c24094e42f6e3e8f0b0c
Murray Cumming <murrayc@murrayc.com>
2012-03-06 22:19:08 +0100

    ReportGenerator: Correct the title positions and use some bold style.
    * src/main/java/org/glom/web/server/ReportGenerator.java:
    Break the code up into reusable functions, correct the placement of
    titles, and use normal/bold styles as in the reports in the desktop
    version.

--- src/main/java/org/glom/web/server/ReportGenerator.java
+++ src/main/java/org/glom/web/server/ReportGenerator.java
@@ -35,6 +35,7 @@ import net.sf.jasperreports.engine.design.JRDesignGroup;
 import net.sf.jasperreports.engine.design.JRDesignQuery;
 import net.sf.jasperreports.engine.design.JRDesignSection;
 import net.sf.jasperreports.engine.design.JRDesignStaticText;
+import net.sf.jasperreports.engine.design.JRDesignStyle;
 import net.sf.jasperreports.engine.design.JRDesignTextField;
 import net.sf.jasperreports.engine.design.JasperDesign;
 import net.sf.jasperreports.engine.export.JRHtmlExporterParameter;
@@ -61,10 +62,16 @@ import org.glom.libglom.Value;
  */
 public class ReportGenerator {
 
-	final int height = 30;
+	final int height = 30; // Points, as specified later.
+	// An arbitrary width, because we must specify _some_ width:
+	final int width = 100; // Points, as specified later.
+
 	LayoutFieldVector fieldsToGet = new LayoutFieldVector();
 	SortClause sortClause = new SortClause();
 	String localeID;
+
+	JRDesignStyle normalStyle = new JRDesignStyle();
+	JRDesignStyle boldStyle = new JRDesignStyle();
 
 	ReportGenerator(final String localeID) {
 		this.localeID = StringUtils.defaultString(localeID);
@@ -82,6 +89,22 @@ public class ReportGenerator {
 
 		final JasperDesign design = new JasperDesign();
 		design.setName(reportName); // TODO: Actually, we want the title.
+
+		normalStyle.setName("Sans_Normal");
+		normalStyle.setDefault(true);
+		normalStyle.setFontName("DejaVu Sans");
+		normalStyle.setFontSize(12);
+		boldStyle.setName("Sans_Bold");
+		boldStyle.setFontName("DejaVu Sans");
+		boldStyle.setFontSize(12);
+		boldStyle.setBold(true);
+		try {
+			design.addStyle(normalStyle);
+			design.addStyle(boldStyle);
+		} catch (final JRException ex) {
+			// TODO Auto-generated catch block
+			ex.printStackTrace();
+		}
 
 		final JRDesignBand titleBand = new JRDesignBand();
 		titleBand.setHeight(height);
@@ -181,7 +204,7 @@ public class ReportGenerator {
 			final LayoutGroup libglomLayoutGroup = LayoutGroup.cast_dynamic(libglomLayoutItem);
 			final LayoutItem_Field libglomLayoutItemField = LayoutItem_Field.cast_dynamic(libglomLayoutItem);
 			if (libglomLayoutItemField != null) {
-				x = addFieldToBand(design, parentBand, headerBand, x, libglomLayoutItemField);
+				x = addFieldToDetailBand(design, parentBand, headerBand, x, libglomLayoutItemField);
 			} else if (libglomLayoutGroup != null) {
 				final LayoutItem_GroupBy libglomGroupBy = LayoutItem_GroupBy.cast_dynamic(libglomLayoutGroup);
 				if (libglomGroupBy != null) {
@@ -203,17 +226,8 @@ public class ReportGenerator {
 					group.setName(fieldName);
 
 					// Show the field value:
-					final JRDesignExpression expression = new JRDesignExpression();
-					expression.setText("$F{" + fieldName + "}");
+					final JRDesignExpression expression = createFieldExpression(fieldName);
 					group.setExpression(expression);
-
-					final JRDesignBand groupBand = new JRDesignBand();
-					groupBand.setHeight(height);
-					((JRDesignSection) group.getGroupHeaderSection()).addBand(groupBand);
-
-					final JRDesignBand footerBand = new JRDesignBand();
-					footerBand.setHeight(height);
-					((JRDesignSection) group.getGroupFooterSection()).addBand(footerBand);
 
 					try {
 						design.addGroup(group);
@@ -223,12 +237,21 @@ public class ReportGenerator {
 					}
 
 					// Show the group-by field:
-					final int groupX = addFieldToBand(design, groupBand, headerBand, x, fieldGroupBy);
+					final JRDesignBand groupBand = new JRDesignBand();
+					groupBand.setHeight(height);
+					((JRDesignSection) group.getGroupHeaderSection()).addBand(groupBand);
+
+					/*
+					 * final JRDesignBand footerBand = new JRDesignBand(); footerBand.setHeight(height);
+					 * ((JRDesignSection) group.getGroupFooterSection()).addBand(footerBand);
+					 */
+
+					final int groupX = addFieldToGroupBand(design, groupBand, x, fieldGroupBy);
 
 					// Show the secondary fields:
 					final LayoutGroup groupSecondaries = libglomGroupBy.get_group_secondary_fields();
 					if (groupSecondaries != null)
-						addToReport(groupSecondaries, design, groupBand, headerBand, groupX);
+						addSecondaryFieldsToGroupBand(groupSecondaries, design, groupBand, groupX);
 				}
 
 				// Recurse into sub-groups:
@@ -239,6 +262,41 @@ public class ReportGenerator {
 		return x;
 	}
 
+	private int addSecondaryFieldsToGroupBand(final org.glom.libglom.LayoutGroup layout_group,
+			final JasperDesign design, final JRDesignBand groupBand, int x) {
+		final LayoutItemVector layoutItemsVec = layout_group.get_items();
+		final int numItems = Utils.safeLongToInt(layoutItemsVec.size());
+		for (int i = 0; i < numItems; i++) {
+			final org.glom.libglom.LayoutItem libglomLayoutItem = layoutItemsVec.get(i);
+
+			final LayoutGroup libglomLayoutGroup = LayoutGroup.cast_dynamic(libglomLayoutItem);
+			final LayoutItem_Field libglomLayoutItemField = LayoutItem_Field.cast_dynamic(libglomLayoutItem);
+			if (libglomLayoutItemField != null) {
+				x = addFieldToGroupBand(design, groupBand, x, libglomLayoutItemField);
+			} else if (libglomLayoutGroup != null) {
+				// We do not expect LayoutItem_GroupBy in the secondary fields:
+				// final LayoutItem_GroupBy libglomGroupBy = LayoutItem_GroupBy.cast_dynamic(libglomLayoutGroup);
+
+				// Recurse into sub-groups:
+				x = addSecondaryFieldsToGroupBand(libglomLayoutGroup, design, groupBand, x);
+			}
+		}
+
+		return x;
+	}
+
+	/**
+	 * @param fieldName
+	 * @return
+	 */
+	private JRDesignExpression createFieldExpression(final String fieldName) {
+		final JRDesignExpression expression = new JRDesignExpression();
+
+		// TODO: Where is this format documented?
+		expression.setText("$F{" + fieldName + "}");
+		return expression;
+	}
+
 	/**
 	 * @param design
 	 * @param parentBand
@@ -246,24 +304,52 @@ public class ReportGenerator {
 	 * @param libglomLayoutItemField
 	 * @return
 	 */
-	private int addFieldToBand(final JasperDesign design, final JRDesignBand parentBand, final JRDesignBand headerBand,
-			int x, final LayoutItem_Field libglomLayoutItemField) {
+	private int addFieldToDetailBand(final JasperDesign design, final JRDesignBand parentBand,
+			final JRDesignBand headerBand, int x, final LayoutItem_Field libglomLayoutItemField) {
 		final String fieldName = addField(design, libglomLayoutItemField);
 
-		// An arbitrary width, because we must specify _some_ width:
-		final int width = 100; // Points, as specified later.
-
 		// Show the field title:
-		final JRDesignStaticText textFieldColumn = new JRDesignStaticText();
-		textFieldColumn.setText(libglomLayoutItemField.get_title(this.localeID));
-		textFieldColumn.setY(0);
-		textFieldColumn.setX(x);
-		textFieldColumn.setWidth(width); // No data will be shown without this.
-		// textFieldColumn.setStretchWithOverflow(true);
-		textFieldColumn.setHeight(height); // We must specify _some_ height.
+		final JRDesignStaticText textFieldColumn = createFieldTitleElement(x, libglomLayoutItemField, false);
+		textFieldColumn.setStyle(boldStyle);
 		headerBand.addElement(textFieldColumn);
 
 		// Show an instance of the field (the field value):
+		final JRDesignTextField textField = createFieldValueElement(x, fieldName);
+		textField.setStyle(normalStyle);
+		parentBand.addElement(textField);
+
+		x += width;
+		return x;
+	}
+
+	private int addFieldToGroupBand(final JasperDesign design, final JRDesignBand parentBand, int x,
+			final LayoutItem_Field libglomLayoutItemField) {
+		final String fieldName = addField(design, libglomLayoutItemField);
+
+		// Show the field title:
+		final JRDesignStaticText textFieldColumn = createFieldTitleElement(x, libglomLayoutItemField, true);
+
+		// Instead, the field value will be bold, because here it is like a title.
+		textFieldColumn.setStyle(normalStyle);
+
+		parentBand.addElement(textFieldColumn);
+		x += width;
+
+		// Show an instance of the field (the field value):
+		final JRDesignTextField textField = createFieldValueElement(x, fieldName);
+		parentBand.addElement(textField);
+		textField.setStyle(boldStyle);
+
+		x += width;
+		return x;
+	}
+
+	/**
+	 * @param x
+	 * @param fieldName
+	 * @return
+	 */
+	private JRDesignTextField createFieldValueElement(final int x, final String fieldName) {
 		final JRDesignTextField textField = new JRDesignTextField();
 
 		// Make sure this field starts at the right of the previous field,
@@ -271,20 +357,40 @@ public class ReportGenerator {
 		textField.setY(0);
 		textField.setX(x);
 		textField.setWidth(width); // No data will be shown without this.
-		x += width;
 
 		// This only stretches vertically, but that is better than
 		// nothing.
 		textField.setStretchWithOverflow(true);
 		textField.setHeight(height); // We must specify _some_ height.
 
-		// TODO: Where is this format documented?
-		final JRDesignExpression expression = new JRDesignExpression();
-		expression.setText("$F{" + fieldName + "}");
+		final JRDesignExpression expression = createFieldExpression(fieldName);
 
 		textField.setExpression(expression);
-		parentBand.addElement(textField);
-		return x;
+		return textField;
+	}
+
+	/**
+	 * @param x
+	 * @param libglomLayoutItemField
+	 * @return
+	 */
+	private JRDesignStaticText createFieldTitleElement(final int x, final LayoutItem_Field libglomLayoutItemField,
+			final boolean withColon) {
+		final JRDesignStaticText textFieldColumn = new JRDesignStaticText();
+
+		String title = StringUtils.defaultString(libglomLayoutItemField.get_title(this.localeID));
+
+		// If the title is at the left, instead of above, we need a : to show that it's a title.
+		if (withColon)
+			title += ":";
+
+		textFieldColumn.setText(title);
+		textFieldColumn.setY(0);
+		textFieldColumn.setX(x);
+		textFieldColumn.setWidth(width); // No data will be shown without this.
+		// textFieldColumn.setStretchWithOverflow(true);
+		textFieldColumn.setHeight(height); // We must specify _some_ height.
+		return textFieldColumn;
 	}
 
 	/**
