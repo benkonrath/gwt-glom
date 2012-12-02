fefd531784926d911af36229c6bf67ce0f5b4ea7
Murray Cumming <murrayc@murrayc.com>
2012-12-02 22:42:57 +0100

    DataItemTest: Test the date type too, and improve the asserts.
            * src/test/java/org/glom/web/shared/DataItemTest.java:
    Add testdate(). See the comment about the awkward server-side-only
    Date API.

--- src/test/java/org/glom/web/shared/DataItemTest.java
+++ src/test/java/org/glom/web/shared/DataItemTest.java
@@ -1,6 +1,11 @@
 package org.glom.web.shared;
 
 import static org.junit.Assert.*;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
 import org.junit.Test;
 
 public class DataItemTest {
@@ -12,7 +17,7 @@ public class DataItemTest {
 	public void testBoolean() {
 		final DataItem item = new DataItem();
 		item.setBoolean(true);
-		assertTrue(item.getBoolean());
+		assertEquals(true, item.getBoolean());
 	}
 
 	@Test
@@ -20,7 +25,7 @@ public class DataItemTest {
 		final DataItem item = new DataItem();
 		final double val = 123.456;
 		item.setNumber(val);
-		assertTrue(item.getNumber() == val);
+		assertEquals(val, item.getNumber(), 0.000001);
 	}
 
 	@Test
@@ -28,7 +33,31 @@ public class DataItemTest {
 		final DataItem item = new DataItem();
 		final String val = "abc";
 		item.setText(val);
-		assertTrue(item.getText() == val);
+		assertEquals(val, item.getText());
+	}
+	
+	@Test
+	public void testDate() {
+		final DataItem item = new DataItem();
+		
+		//Not that we use a different API to create a Date
+		//in the client-side test (GwtTestDataItem),
+		//because this API is not available in GWT client code.
+		//TODO: Is there no easier (and non-deprecated) way to create
+		//a Date instance?
+		Calendar cal = new GregorianCalendar();
+		cal.setTime(new Date());
+		cal.set(Calendar.HOUR_OF_DAY, 0);
+		cal.set(Calendar.MINUTE, 0);
+		cal.set(Calendar.SECOND, 0);
+		cal.set(Calendar.MILLISECOND, 0);
+		cal.set(Calendar.YEAR, 1973);
+		cal.set(Calendar.MONTH, 4); /* The month is 0 indexed. */
+		cal.set(Calendar.DAY_OF_MONTH, 11);
+		final Date val = cal.getTime();
+				
+		item.setDate(val);
+		assertEquals(val, item.getDate());
 	}
 
 }
