f8f0f19470ce19cf264ac55a7420ccda1c270d33
Murray Cumming <murrayc@murrayc.com>
2013-06-19 08:16:47 +0200

    Add GlomPlace.
    * src/main/java/org/glom/web/client/place/HasDocumentPlace.java:
      Move most of the Tokenizer to a parent class, so we can have
      system-wide URLs that do not involve a particular document.

---
+++ src/main/java/org/glom/web/client/place/GlomPlace.java
@@ -0,0 +1,113 @@
+/*
+ * Copyright (C) 2013 Openismus GmbH
+ *
+ * This file is part of GWT-Glom.
+ *
+ * GWT-Glom is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * GWT-Glom is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with GWT-Glom.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.glom.web.client.place;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.glom.web.client.StringUtils;
+
+import com.google.gwt.place.shared.Place;
+
+/**
+ * A derived GWT Place with a Tokenizer that
+ * can parse and build the URL's token parameters.
+ * TODO: These are commonly re-implemented. Ask for them to be added to GWT? Are they in GWP?
+ * 
+ * @author Murray Cumming <murrayc@openismus.com>
+ *
+ */
+public abstract class GlomPlace extends Place {
+
+	public static class Tokenizer {
+		private final String equals = "=";
+		private final String separator = "&";
+		
+		/**
+		 * Get a HashMap of parameter names and values from the history token. This can parse tokens built by
+		 * buildParamsToken().
+		 * 
+		 * @param historyToken
+		 *            The historyToken provided to getPlace().
+		 * @return A HasMap of names to values.
+		 */
+		protected HashMap<String, String> getTokenParams(final String historyToken) {
+			final String[] arStr = historyToken.substring(0, historyToken.length()).split(separator);
+			final HashMap<String, String> params = new HashMap<String, String>();
+			for (final String element : arStr) {
+				final String[] substr = element.split(equals);
+				if (substr.length != 2) {
+					continue;
+				}
+	
+				String key = "";
+				String value = "";
+				if (!StringUtils.isEmpty(substr[0])) {
+					key = substr[0];
+				}
+	
+				if (!StringUtils.isEmpty(substr[1])) {
+					value = substr[1];
+				}
+	
+				if (!StringUtils.isEmpty(key) && !StringUtils.isEmpty(value)) {
+					params.put(key, value);
+				}
+			}
+	
+			return params;
+		}
+	
+		/**
+		 * Build a history token based on a HashMap of parameter names and values. This can later be parsed by
+		 * getTokenParams().
+		 * 
+		 * @param params
+		 *            A HashMap of names and values.
+		 * @return A history string for use by getToken() implementation.
+		 */
+		protected String buildParamsToken(final HashMap<String, String> params) {
+			String token = "";
+			for (final Map.Entry<String, String> entry : params.entrySet()) {
+				final String key = entry.getKey();
+				final String value = entry.getValue();
+				if (StringUtils.isEmpty(key) || StringUtils.isEmpty(value)) {
+					continue;
+				}
+	
+				if (token != "") {
+					token += separator;
+				}
+	
+				token += key + equals + value;
+			}
+	
+			return token;
+		}
+	}
+
+	/**
+	 * 
+	 */
+	public GlomPlace() {
+		super();
+	}
+
+}
--- src/main/java/org/glom/web/client/place/HasDocumentPlace.java
+++ src/main/java/org/glom/web/client/place/HasDocumentPlace.java
@@ -19,20 +19,17 @@
 
 package org.glom.web.client.place;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import org.glom.web.client.StringUtils;
-
-import com.google.gwt.place.shared.Place;
-
 /**
  * @author Murray Cumming <murrayc@murrayc.com>
  *
  */
-public abstract class HasDocumentPlace extends Place {
+public abstract class HasDocumentPlace extends GlomPlace {
 
 	protected final String documentID;
+	
+	public static class Tokenizer extends GlomPlace.Tokenizer {
+		protected final String documentKey = "document";
+	}
 
 	/**
 	 * 
@@ -44,74 +41,6 @@ public abstract class HasDocumentPlace extends Place {
 	
 	public String getDocumentID() {
 		return documentID;
-	}
-	
-	public static class Tokenizer {
-		protected final String documentKey = "document";
-		private final String equals = "=";
-		private final String separator = "&";
-		
-		/**
-		 * Get a HashMap of parameter names and values from the history token. This can parse tokens built by
-		 * buildParamsToken().
-		 * 
-		 * @param historyToken
-		 *            The historyToken provided to getPlace().
-		 * @return A HasMap of names to values.
-		 */
-		protected HashMap<String, String> getTokenParams(final String historyToken) {
-			final String[] arStr = historyToken.substring(0, historyToken.length()).split(separator);
-			final HashMap<String, String> params = new HashMap<String, String>();
-			for (final String element : arStr) {
-				final String[] substr = element.split(equals);
-				if (substr.length != 2) {
-					continue;
-				}
-
-				String key = "";
-				String value = "";
-				if (!StringUtils.isEmpty(substr[0])) {
-					key = substr[0];
-				}
-
-				if (!StringUtils.isEmpty(substr[1])) {
-					value = substr[1];
-				}
-
-				if (!StringUtils.isEmpty(key) && !StringUtils.isEmpty(value)) {
-					params.put(key, value);
-				}
-			}
-
-			return params;
-		}
-
-		/**
-		 * Build a history token based on a HashMap of parameter names and values. This can later be parsed by
-		 * getTokenParams().
-		 * 
-		 * @param params
-		 *            A HashMap of names and values.
-		 * @return A history string for use by getToken() implementation.
-		 */
-		protected String buildParamsToken(final HashMap<String, String> params) {
-			String token = "";
-			for (final Map.Entry<String, String> entry : params.entrySet()) {
-				final String key = entry.getKey();
-				final String value = entry.getValue();
-				if (StringUtils.isEmpty(key) || StringUtils.isEmpty(value)) {
-					continue;
-				}
-
-				if (token != "") {
-					token += separator;
-				}
-
-				token += key + equals + value;
-			}
-
-			return token;
-		}
 	}
 
 }
